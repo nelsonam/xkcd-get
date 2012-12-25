92e5b8e6bb6af78ea7b3fb6cc4880c8358176b7a
Allison Nelson <nelsonam@appstate.edu>
2012-12-24 20:30:24 -0500

    updated xkcd-get.sh to get multiple image types

--- xkcd-get.sh
+++ xkcd-get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir comics; cd comics
-for i in {1..1150}; do
-url=`lynx -dump http://www.xkcd.com/$i | grep jpg`
+for i in {1..1151}; do
+url=`lynx -dump http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
 wget $url
 done
