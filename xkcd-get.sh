518b393bd0e3275b124f4ae63fcaaba57816c781
Allison Nelson <nelsonam@appstate.edu>
2012-12-26 23:24:11 -0500

    we can figure out what the latest comic is now. still downloads them all at once though.

--- xkcd-get.sh
+++ xkcd-get.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#makes a folder for the comicz
 mkdir comics; cd comics
-for i in {1..1151}; do
+#gets the latest number
+latest=`lynx -dump http://www.xkcd.com | grep Permanent | tr "/" "\n" | sed -n '4p'`
+#gotta download 'em all!
+for i in {1..$latest}; do
+#getting the url
 url=`lynx -dump http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
 wget $url
 done
