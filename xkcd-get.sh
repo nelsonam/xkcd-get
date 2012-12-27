f58c1048ce65fdfd70ef6b9b364bf67924dc0906
Allison Nelson <nelsonam@appstate.edu>
2012-12-27 14:22:02 -0500

    fixed loop and rename files based on number

--- xkcd-get.sh
+++ xkcd-get.sh
@@ -4,8 +4,8 @@ mkdir comics; cd comics
 #gets the latest number
 latest=`lynx -dump http://www.xkcd.com | grep Permanent | tr "/" "\n" | sed -n '4p'`
 #gotta download 'em all!
-for i in {1..$latest+1}; do
-#getting the url
-url=`lynx -dump http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
-wget $url
+for ((i=1;i<=latest;i++)); do
+    #getting the url
+    url=`lynx -dump http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
+    wget $url -O "$i.jpg"
 done
