1a38545ca12cfdbfabd747787f7c343cf3d48a2d
John Pham <jpham4@gmu.edu>
2012-12-27 21:56:55 -0500

    Modified to remove dependency on lynx

--- xkcd-get.sh
+++ xkcd-get.sh
@@ -2,10 +2,12 @@
 #makes a folder for the comicz
 mkdir comics; cd comics
 #gets the latest number
-latest=`lynx -dump http://www.xkcd.com | grep Permanent | tr "/" "\n" | sed -n '4p'`
+latest=`wget -O- http://www.xkcd.com | grep Permanent | tr "/" "\n" | sed -n '4p'`
 #gotta download 'em all!
 for ((i=1;i<=latest;i++)); do
-    #getting the url
-    url=`lynx -dump http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
+    #getting the link (and other junk)
+    link=`wget -O- http://www.xkcd.com/$i | grep http://imgs.xkcd.com/comics`
+    #getting the URL
+    url=`echo $link|grep -o 'src="[^"]*"'|sed -e 's/src="\([^"]*\)"/\1/'`
     wget $url -O "$i.jpg"
 done
